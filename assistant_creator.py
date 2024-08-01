3d06cdb90ffb9308a1aaa92327a81c4596e44416
CP-PSH <pksd@coinplug.com>
2024-08-01 16:32:47 +0900

    Initial commit

---
+++ assistant_creator.py
@@ -0,0 +1,51 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv('API_KEY')
+print(api_key)
+client = OpenAI(api_key=api_key)
+
+file_path = "file/web2x.pdf"
+
+def fileUpload(file_path):
+    file = client.files.create(
+        file=open(file_path, "rb"),
+        purpose="assistants"
+    )
+    return file.id  # 파일 ID 반환
+
+
+# 파일 업로드 및 ID 획득
+# uploaded_file_id = fileUpload(file_path)
+# 벡터 스토어 생성 및 ID 획득
+
+# vector_store = client.beta.vector_stores.create(
+#   name="LH GUIDE Support FAQ"
+# )
+
+# vector_store_file = client.beta.vector_stores.files.create(
+#   vector_store_id=vector_store.id,
+#   file_id=uploaded_file_id
+# )
+
+def assistant_creator():
+    my_assistant = client.beta.assistants.create(
+        instructions="이 챗봇은 WEB2X 문서기반으로 질의응답하는 bot.",
+        name="web2x Helper",
+        tools=[{"type": "file_search"}],
+        tool_resources={"file_search": {"vector_store_ids": [vector_store.id]}},
+        model="gpt-4o"
+      # gpt-4o	
+        
+    )
+    return my_assistant.id
+
+# print(assistant_creator())
+
+
+# my_assistant_id = assistant_creator()
+
+# empty_thread = client.beta.threads.create()
+
---
+++ test.py
@@ -0,0 +1,82 @@
+from openai import OpenAI
+import streamlit as st
+import time
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv('API_KEY')
+assistant_id = os.getenv('ASSISTANT_ID')
+
+client = OpenAI(api_key=api_key)
+
+# thread_id = "thread_UfwfQO5f0mIlVLMheDLYKR9u"
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password",value=api_key)
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+    # "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+    thread_id = st.text_input('Thread ID')
+    thread_btn = st.button("Create a new Thread")
+    if thread_btn:
+        thread = client.beta.threads.create()
+        thread_id = thread.id
+
+        st.subheader(f"{thread_id}",divider="rainbow")
+        st.info("스레드 생성 완")
+st.title("💬 TEST: WEB2X Q&A ChatBot" )
+st.caption("🚀 A Streamlit chatbot powered by OpenAI")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "무엇이든 물어보세용 : WEB2X"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+        
+    if not thread_id:
+        st.info("Please add your thread_id to continue.")
+        st.stop()    
+
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+
+    response = client.beta.threads.messages.create(
+    thread_id,
+    role="user",
+    content=prompt, 
+    )
+
+    run = client.beta.threads.runs.create(
+    thread_id=thread_id,
+    assistant_id=assistant_id
+    )
+    run_id = run.id
+
+    while True:
+        run = client.beta.threads.runs.retrieve(
+        thread_id=thread_id,
+        run_id=run_id
+        )
+        if run.status == "completed":
+            break
+        else:
+            time.sleep(2)
+
+    thread_messages = client.beta.threads.messages.list(thread_id)
+
+    msg=thread_messages.data[0].content[0].text.value
+
+    st.session_state.messages.append({"role": "assistant", "content": msg})
+    st.chat_message("assistant").write(msg)
+
+    # response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    # msg = response.choices[0].message.content
+  
+
+    
+        
