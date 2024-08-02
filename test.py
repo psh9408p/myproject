cbf1b87e0264d22975cdebf2980edd28342e84d8
CP-PSH <psh9408p@naver.com>
2024-08-02 10:46:59 +0900

    :fix: assistant_id  secrets

--- test.py
+++ test.py
@@ -24,13 +24,15 @@ load_dotenv()
 
 OPENAI_API_KEY = st.secrets["openai"]["api_key"]
 # ASSISTANT_ID = st.secrets["openai"]["assistant_id"]
-ASSISTANT_ID =  st.secrets.get("openai", {}).get("assistant_id")
+ASSISTANT_ID =  st.secrets["openai"]["assistant_id"]
 
 if ASSISTANT_ID is None:
     st.error("Assistant ID is not set in secrets.toml")
 else:
     st.write("Assistant ID:", ASSISTANT_ID)
+    
 # API 키 확인
+st.write(OPENAI_API_KEY)
 if not OPENAI_API_KEY:
     st.error("OpenAI API key is not set. Please set it in your environment variables or Streamlit secrets.")
     st.stop()
